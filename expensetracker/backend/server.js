ffa885cea0e691b02f066c8e2fceddb42d339ed0
ggarrreto <gstoneg616@gmail.com>
2023-11-02 18:03:39 -0700

    route expenses and users to server.js

--- expensetracker/backend/server.js
+++ expensetracker/backend/server.js
@@ -18,6 +18,12 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
+const expensesRouter = require('./routes/expenses');
+const usersRouter = require('./routes/users');
+
+app.use('/expenses', expensesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log('Server is running on port: ${port}');
 });
