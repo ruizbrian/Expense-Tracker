45e238a6710a60ad3b641744a9d882ce353f4978
Brian Ruiz <143536677+ruizbrian@users.noreply.github.com>
2023-11-09 15:02:43 -0800

    added delete, update, and get from ID

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -3,8 +3,8 @@ let Expense = require('../models/expense.model');
 
 router.route('/').get((req, res) => {
     Expense.find()
-    .then(expenses => res.json(expenses))
-    .catch(err => res.status(400).json('Error: ' + err));
+        .then(expenses => res.json(expenses))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -21,8 +21,34 @@ router.route('/add').post((req, res) => {
     });
 
     newExpense.save()
-    .then(() => res.json('Expense added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+        .then(() => res.json('Expense added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Expense.findById(req.params.id)
+        .then(expense => res.json(expense))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Expense.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Expense deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Expense.findById(req.params.id)
+        .then(expense => {
+            expense.username = req.body.username;
+            expense.description = req.body.description;
+            expense.expense = Number(req.body.expense);
+            expense.date = Date.parse(req.body.date);
+
+            expense.save()
+                .then(() => res.json('Expense updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
