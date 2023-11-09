7f9956711dba71e609a9f0f9ea7b02b4db8f28be
Brian Ruiz <143536677+ruizbrian@users.noreply.github.com>
2023-11-08 20:55:13 -0800

    edited mongoose error handling

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri)
+    .then(() => {
+        console.log("MongoDB database connection established successfully");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
 
 // const expensesRouter = require('./routes/expenses');
 // const usersRouter = require('./routes/users');
@@ -25,5 +28,5 @@ connection.once('open', () => {
 // app.use('/users', usersRouter);
 
 app.listen(port, () => {
-    console.log('Server is running on port: ${port}');
+    console.log(`Server is running on port: ${port}`);
 });
