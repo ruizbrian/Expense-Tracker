06998ef945bd193632d5fe9c5abf6ce90b693676
ggarrreto <gstoneg616@gmail.com>
2023-11-10 01:14:21 -0800

    add users into front end

--- frontend/src/components/create-expense.component.js
+++ frontend/src/components/create-expense.component.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react';
 
 export default class CreateExpense extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeUsername.bind(this);
+        this.onChangeDuration = this.onChangeUsername.bind(this);
+        this.onChangeDate = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username = '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    onChangeUsername(e) {
+        this.state({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.state({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.state({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.state({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const expense = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(expense)
+
+        window.location = "/";
+    }
+
     render() {
         return (
             <div>
