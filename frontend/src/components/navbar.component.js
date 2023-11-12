c8d4fe7e250651f0f63f82aa7f320e1800b4f5ec
Brian Ruiz <143536677+ruizbrian@users.noreply.github.com>
2023-11-12 15:20:37 -0800

    fixed garretts mistakes

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -5,19 +5,19 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="nabar nabar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">ExpenTracker</Link>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">Expense Tracker</Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
-                        <Link to="/" className="nav-link">Expense</Link>
-                    </li>
-                    <li className="navbar-item">
-                        <Link to="/create" className="nav-link">Create Expense Log</Link>
-                    </li>
-                    <li className="navbar-item">
-                        <Link to="/user" className="nav-link"> Create User</Link>
-                    </li>
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">Expense</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Create Expense Log</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/user" className="nav-link"> Create User</Link>
+                        </li>
                     </ul>
                 </div>
 
