3c2ab390e7dbf8ecd974d50144f5e6f0e13af596
ggarrreto <gstoneg616@gmail.com>
2023-11-12 23:18:16 -0800

    complete frontend to backend connection

--- frontend/src/components/edit-expense.component.js
+++ frontend/src/components/edit-expense.component.js
@@ -1,10 +1,177 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"
 
 export default class EditExpense extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeExpense = this.onChangeExpense.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            expense: '',
+            date: new Date(),
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/expense/'+this.props.match.params.id)
+        .then(response => {
+            this.setState({
+                username: response.data.username,
+                description: response.data.description,
+                expense: response.data.expense,
+                date: new Date(response.data.date)
+            })
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        })
+
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                    })
+                }
+            })
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeExpense(e) {
+        let value = e.target.value;
+
+        // Remove non-numeric characters
+        value = value.replace(/[^0-9.]/g, '');
+
+        // Check if it's a valid number
+        if (!isNaN(parseFloat(value))) {
+            // Update the state without formatting to .00
+            this.setState({
+                expense: value
+            });
+        } else {
+            // Set to empty string if not a valid number
+            this.setState({
+                expense: ''
+            });
+        }
+    }
+
+    onBlurExpense() {
+        // Add ".00" if the value is a whole number
+        if (this.state.expense !== '' && !this.state.expense.includes('.')) {
+            this.setState({
+                expense: parseFloat(this.state.expense).toFixed(2)
+            });
+        }
+    }
+
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const expense = {
+            username: this.state.username,
+            description: this.state.description,
+            expense: this.state.expense !== '' ? parseFloat(this.state.expense) : 0,
+            date: this.state.date
+        }
+
+        console.log(expense)
+
+        axios.post('http://localhost:5000/expense/update/'+this.props.match.params.id, expense)
+        .then(res => console.log(res.data));
+
+        window.location = "/";
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Edit Expense component!</p>
+                <h3>Edit Expense Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select
+                            ref="userInput"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        >
+                            {this.state.users.map(function (user) {
+                                return (
+                                    <option key={user} value={user}>
+                                        {user}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Expense (in Dollars): </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.expense}
+                            onChange={this.onChangeExpense}
+                            onBlur={this.onBlurExpense.bind(this)}
+                            placeholder="Enter expense amount"
+                        />
+
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
--- frontend/src/components/expense-list.component.js
+++ frontend/src/components/expense-list.component.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Expense = props => (
+    <tr>
+        <td>{props.expense.username}</td>
+        <td>{props.expense.description}</td>
+        <td>{props.expense.expense}</td>
+        <td>{props.expense.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props.exercise._id) }}>delete</a>
+        </td>
+    </tr>
+)
 
 export default class ExpenseList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteExpense = this.deleteExpense.bind(this);
+
+        this.state = {exercise: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/expense/')
+            .then(response => {
+                this.setState({ expense: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    deleteExpense(id) {
+        axios.delete('http://localhost:5000/expense/'+id)
+        .then(res => console.log(res.data));
+
+        this.setState({
+            expense: this.state.expense.filter(el => el._id !== id)
+        })
+    }
+
+    expenseList() {
+        return this.state.expense.map(currentexpense => {
+            return <Expense expense={currentexpense} deleteExpense={this.deleteExpense} key={currentexpense._id}/>;
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Expense List component!</p>
+                <h3>Logged Expenses</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Expense</th>
+                            <th>Date</th>
+                            <th>Category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.exerciseList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
