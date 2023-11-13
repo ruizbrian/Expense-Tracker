c6a8b889b18697074ea5a468a406f0ee096e148a
Brian Ruiz <143536677+ruizbrian@users.noreply.github.com>
2023-11-12 18:07:47 -0800

    Renamed durtaion func to exp & formated expenses

--- frontend/src/components/create-expense.component.js
+++ frontend/src/components/create-expense.component.js
@@ -8,14 +8,14 @@ export default class CreateExpense extends Component {
 
         this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangeDescription = this.onChangeDescription.bind(this);
-        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeExpense = this.onChangeExpense.bind(this);
         this.onChangeDate = this.onChangeDate.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
-            username : '',
+            username: '',
             description: '',
-            duration: 0,
+            expense: '',
             date: new Date(),
             users: []
         }
@@ -40,11 +40,35 @@ export default class CreateExpense extends Component {
         });
     }
 
-    onChangeDuration(e) {
-        this.setState({
-            duration: e.target.value
-        });
+    onChangeExpense(e) {
+        let value = e.target.value;
+    
+        // Remove non-numeric characters
+        value = value.replace(/[^0-9.]/g, '');
+    
+        // Check if it's a valid number
+        if (!isNaN(parseFloat(value))) {
+            // Update the state without formatting to .00
+            this.setState({
+                expense: value
+            });
+        } else {
+            // Set to empty string if not a valid number
+            this.setState({
+                expense: ''
+            });
+        }
     }
+    
+    onBlurExpense() {
+        // Add ".00" if the value is a whole number
+        if (this.state.expense !== '' && !this.state.expense.includes('.')) {
+            this.setState({
+                expense: parseFloat(this.state.expense).toFixed(2)
+            });
+        }
+    }
+    
 
     onChangeDate(date) {
         this.setState({
@@ -58,12 +82,15 @@ export default class CreateExpense extends Component {
         const expense = {
             username: this.state.username,
             description: this.state.description,
-            duration: this.state.duration,
+            expense: this.state.expense !== '' ? parseFloat(this.state.expense) : 0,
             date: this.state.date
         }
 
         console.log(expense)
 
+        // Add logic to handle the submission, e.g., send data to server or update state
+
+        // For now, let's redirect to "/"
         window.location = "/";
     }
 
@@ -74,38 +101,43 @@ export default class CreateExpense extends Component {
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Username: </label>
-                        <select ref="userInput"
+                        <select
+                            ref="userInput"
                             required
                             className="form-control"
                             value={this.state.username}
-                            onChange={this.onChangeUsername}>
-                                {
-                                    this.state.users.map(function(user) {
-                                        return <option
-                                        key={user}
-                                        value={user}>{user}
-                                        </option>;
-                                    })
-                                }
-                            </select>
+                            onChange={this.onChangeUsername}
+                        >
+                            {this.state.users.map(function (user) {
+                                return (
+                                    <option key={user} value={user}>
+                                        {user}
+                                    </option>
+                                );
+                            })}
+                        </select>
                     </div>
                     <div className="form-group">
                         <label>Expense (in dollars): </label>
-                        <input 
+                        <input
                             type="text"
                             className="form-control"
-                            value={this.state.duration}
-                            onChange={this.onChangeDuration}
-                            />
+                            value={this.state.expense}
+                            onChange={this.onChangeExpense}
+                            onBlur={this.onBlurExpense.bind(this)}
+                            placeholder="Enter expense amount"
+                        />
+
                     </div>
                     <div className="form-group">
                         <label>Description: </label>
-                        <input type="text"
-                            required 
+                        <input
+                            type="text"
+                            required
                             className="form-control"
                             value={this.state.description}
                             onChange={this.onChangeDescription}
-                            />
+                        />
                     </div>
                     <div className="form-group">
                         <label>Date: </label>
@@ -113,7 +145,7 @@ export default class CreateExpense extends Component {
                             <DatePicker
                                 selected={this.state.date}
                                 onChange={this.onChangeDate}
-                                />
+                            />
                         </div>
                     </div>
 
