24fe57691d7b27d4936fec40cd07d8146d77f6e5
ruizbrian <bruiz26@csub.edu>
2023-12-16 14:33:45 -0800

    Added username taken

--- frontend/src/components/create-user.component.js
+++ frontend/src/components/create-user.component.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
     constructor(props) {
@@ -9,13 +9,15 @@ export default class CreateUser extends Component {
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
-            username: '',
-        }
+            username: "",
+            error: "",
+        };
     }
 
     onChangeUsername(e) {
         this.setState({
-            username: e.target.value
+            username: e.target.value,
+            error: "", // Clear any previous error when the user starts typing again
         });
     }
 
@@ -24,26 +26,52 @@ export default class CreateUser extends Component {
 
         const user = {
             username: this.state.username,
-        }
+        };
 
-        console.log(user)
-
-        axios.post('http://localhost:5000/users/add', user)
-            .then(res => console.log(res.data));
-
-        this.setState({
-            username: ''
-        })
+        axios
+            .post("http://localhost:5000/users/add", user)
+            .then((res) => {
+                console.log(res.data);
+                // Optionally, you can navigate to a different page or perform other actions on successful registration
+            })
+            .catch((error) => {
+                // Handle registration error
+                if (error.response && error.response.status === 400) {
+                    console.log(
+                        "Username already taken:",
+                        error.response.data.error
+                    );
+                    this.setState({
+                        error: "Username already taken. Please choose another.",
+                    });
+                } else {
+                    console.error(
+                        "Unexpected error during registration:",
+                        error.message
+                    );
+                    this.setState({
+                        error: "An unexpected error occurred. Please try again later.",
+                    });
+                }
+            })
+            .finally(() => {
+                // Clear the username input field regardless of success or failure
+                this.setState({ username: "" });
+            });
     }
 
     render() {
         return (
             <div>
                 <h3>Create New User</h3>
+                {this.state.error && (
+                    <p style={{ color: "red" }}>{this.state.error}</p>
+                )}
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Username: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
                             className="form-control"
                             value={this.state.username}
@@ -51,10 +79,14 @@ export default class CreateUser extends Component {
                         />
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Create User" className="btn btn-primary" />
+                        <input
+                            type="submit"
+                            value="Create User"
+                            className="btn btn-primary"
+                        />
                     </div>
                 </form>
             </div>
-        )
+        );
     }
 }
