04502bdd2238ab3c5f2bd009b6852187bda548ee
ruizbrian <bruiz26@csub.edu>
2023-12-17 20:37:28 -0800

    part of sign up done

---
+++ frontend/src/components/signUp.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function Signup() {
+
+    return ( 
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100"> 
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Register</h2>
+                <form>
+        <div className="mb-3">
+            <label htmlFor="email">
+                <strong>Name</strong>
+            </label>
+            <input
+            type="text"
+            placeholder="Enter Name"
+            autoComplete="off"
+            name="email"
+            className="form-control rounded-0"
+            />
+            </div>
+            <div className="mb-3>
+                <label htmlFor="email">
+                    <strong>Email</strong>
+                </label>
+            <input
+            type="email"
+            placeholder="Enter Email"
+            autoComplete="off"
+            name="email"
+            className="form-control rounded-0"
+            />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email">
+                    <strong>Password</strong>
+                </label>
+                <input 
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                className="form-control rounded-0"
+                />
+            </div>
+            
+            );
+}
+
+export default Signup;
