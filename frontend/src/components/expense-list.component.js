14ea7c87cf262226327a078ac8efba6fe7c4a314
ruizbrian <bruiz26@csub.edu>
2023-12-18 11:45:26 -0800

    added icons and dark mode on list

--- frontend/src/components/expense-list.component.js
+++ frontend/src/components/expense-list.component.js
@@ -3,124 +3,151 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Expense = (props) => {
-  const handleDelete = () => {
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete this expense?"
-    );
-    if (confirmDelete) {
-      props.deleteExpense(props.expense._id);
-    }
-  };
+    const handleDelete = () => {
+        const confirmDelete = window.confirm(
+            "Are you sure you want to delete this expense?"
+        );
+        if (confirmDelete) {
+            props.deleteExpense(props.expense._id);
+        }
+    };
 
-  return (
-    <tr>
-      <td>{props.expense.username}</td>
-      <td>{props.expense.description}</td>
-      <td
-        style={{
-          color: props.expense.type === "income" ? "green" : "red",
-        }}
-      >
-        {props.expense.type === "income" ? "+" : "-"} ${" "}
-        {props.expense.amount}
-      </td>
-      <td>{props.expense.date.substring(0, 10)}</td>
-      <td>
-        <Link to={"/edit/" + props.expense._id}>
-          <button className="btn btn-dark">Edit</button>
-        </Link>{" "}
-        |{" "}
-        <button className="btn btn-danger" onClick={handleDelete}>
-          Delete
-        </button>
-      </td>
-    </tr>
-  );
+    return (
+        <tr>
+            <td>{props.expense.username}</td>
+            <td>{props.expense.description}</td>
+            <td
+                style={{
+                    color: props.expense.type === "income" ? "green" : "red",
+                }}
+            >
+                {props.expense.type === "income" ? "+" : "-"} ${" "}
+                {props.expense.amount}
+            </td>
+            <td>{props.expense.date.substring(0, 10)}</td>
+            <td>
+                <Link to={"/edit/" + props.expense._id}>
+                    <button className="btn btn-dark">Edit</button>
+                </Link>{" "}
+                |{" "}
+                <button className="btn btn-danger" onClick={handleDelete}>
+                    Delete
+                </button>
+            </td>
+        </tr>
+    );
 };
 
 export default class ExpenseList extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.deleteExpense = this.deleteExpense.bind(this);
+        this.deleteExpense = this.deleteExpense.bind(this);
 
-    this.state = { expense: [] };
-  }
+        this.state = { expense: [], darkMode: false };
+    }
 
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/expense/")
-      .then((response) => {
-        // Sort expenses by date in descending order (most recent first)
-        const sortedExpenses = response.data.sort(
-          (a, b) => new Date(b.date) - new Date(a.date)
+    componentDidMount() {
+        axios
+            .get("http://localhost:5000/expense/")
+            .then((response) => {
+                const sortedExpenses = response.data.sort(
+                    (a, b) => new Date(b.date) - new Date(a.date)
+                );
+
+                this.setState({ expense: sortedExpenses });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    deleteExpense(id) {
+        axios
+            .delete("http://localhost:5000/expense/" + id)
+            .then((res) => console.log(res.data));
+
+        this.setState({
+            expense: this.state.expense.filter((el) => el._id !== id),
+        });
+    }
+
+    calculateTotal() {
+        return this.state.expense.reduce(
+            (total, expense) =>
+                total +
+                (expense.type === "income" ? expense.amount : -expense.amount),
+            0
         );
+    }
 
-        this.setState({ expense: sortedExpenses });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+    expenseList() {
+        return this.state.expense.map((currentExpense) => {
+            return (
+                <Expense
+                    expense={currentExpense}
+                    deleteExpense={this.deleteExpense}
+                    key={currentExpense._id}
+                />
+            );
+        });
+    }
 
-  deleteExpense(id) {
-    axios
-      .delete("http://localhost:5000/expense/" + id)
-      .then((res) => console.log(res.data));
+    toggleDarkMode = () => {
+        this.setState((prevState) => ({
+            darkMode: !prevState.darkMode,
+        }));
+    };
 
-    this.setState({
-      expense: this.state.expense.filter((el) => el._id !== id),
-    });
-  }
-
-  // Calculate the total amount by summing up individual expenses
-  calculateTotal() {
-    return this.state.expense.reduce(
-      (total, expense) =>
-        total + (expense.type === "income" ? expense.amount : -expense.amount),
-      0
-    );
-  }
-
-  expenseList() {
-    return this.state.expense.map((currentExpense) => {
-      return (
-        <Expense
-          expense={currentExpense}
-          deleteExpense={this.deleteExpense}
-          key={currentExpense._id}
-        />
-      );
-    });
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <h3>Logged Transactions</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-              <th>Description</th>
-              <th>Transactions</th>
-              <th>Date</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>{this.expenseList()}</tbody>
-          {/* Display the total amount */}
-          <tfoot>
-            <tr>
-              <td colSpan="2"></td>
-              <td>
-                <strong>Total Amount: ${this.calculateTotal().toFixed(2)}</strong>
-              </td>
-              <td colSpan="2"></td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                className={`container-fluid ${
+                    this.state.darkMode ? "bg-dark text-light" : ""
+                }`}
+            >
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                    <h3>Logged Transactions</h3>
+                    <button
+                        className={`btn ${
+                            this.state.darkMode ? "btn-light" : "btn-secondary"
+                        }`}
+                        onClick={this.toggleDarkMode}
+                    >
+                        Toggle Dark Mode
+                    </button>
+                </div>
+                <div className="table-responsive">
+                    <table
+                        className={`table ${
+                            this.state.darkMode ? "table-dark text-light" : ""
+                        }`}
+                    >
+                        <thead className="thead-light">
+                            <tr>
+                                <th>Username</th>
+                                <th>Description</th>
+                                <th>Transactions</th>
+                                <th>Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>{this.expenseList()}</tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan="2"></td>
+                                <td>
+                                    <strong>
+                                        Total Amount: $
+                                        {this.calculateTotal().toFixed(2)}
+                                    </strong>
+                                </td>
+                                <td colSpan="2"></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaPlus, FaUser } from "react-icons/fa"; // Import icons from Font Awesome
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-sm px-3">
-                <Link to="/" className="navbar-brand">Expense Tracker</Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
-                            <Link to="/" className="nav-link">Home</Link>
+            <nav className="navbar fixed-bottom navbar-dark bg-dark p-0">
+                <div className="container d-flex justify-content-center">
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                <FaHome size={20} /> Home
+                            </Link>
                         </li>
-                        <li className="navbar-item">
-                            <Link to="/create" className="nav-link">Add Entry</Link>
+                        <li className="nav-item">
+                            <Link to="/create" className="nav-link">
+                                <FaPlus size={20} /> Add Entry
+                            </Link>
                         </li>
-                        <li className="navbar-item">
-                            <Link to="/user" className="nav-link">Create User</Link>
+                        <li className="nav-item">
+                            <Link to="/user" className="nav-link">
+                                <FaUser size={20} /> Create User
+                            </Link>
                         </li>
                     </ul>
                 </div>
